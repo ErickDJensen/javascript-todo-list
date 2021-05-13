e355c66089eaa4490849f1c874b1c472ac780032
Erick Jensen <Erick.Jensen010@gmail.com>
2021-05-12 21:28:45 -0500

    finished coding the logic to add a todo

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log('Hello World');
+
+const completed = document.querySelector('.completed');
+const deleted = document.querySelector('.deleted');
+const addTodo = document.querySelector('.addTodo');
+const list = document.querySelector('.list');
+
+
+const generateTemplate = todo => {
+    const html = `<li class="todoList">${todo}
+                  <button class="completed">Completed</button>
+                  <button class="delete">Delete</button>
+                  </li>`
+    list.innerHTML += html
+}
+
+addTodo.addEventListener('submit', e => {
+
+    e.preventDefault();
+    const todo = addTodo.addTodo.value.trim();
+    console.log(todo);
+
+    if (todo.length) {
+        generateTemplate(todo);
+        addTodo.reset();
+    };
+
+
+
+});
